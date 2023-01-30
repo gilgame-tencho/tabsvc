c3bab492b762511e5aaa259a4931d2687cad46a5
gilgame-tencho <tokubetugosetuchitai.ttiw@gmail.com>
2023-01-30 22:47:03 +0900

    mod

--- tabsvc.js
+++ tabsvc.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const fl = require('node-filelist');
-// const unzip = require('unzip');
-const {exec} = require('child_process')
+const {exec} = require('child_process');
+const { program } = require("commander");
 
 // const PWD = process.env.PWD;
 const ws_name = 'ws';
@@ -14,8 +14,11 @@ const WS = path.join(__dirname, ws_name);
 const ROOT = path.basename(__dirname);
 const SVC = path.join(__dirname, svc_name);
 
-console.log(WS);
-console.log(ROOT);
+function what_params(){    
+    console.log(WS);
+    console.log(ROOT);
+    console.log(option);
+}
 
 function path_parse(path){
     var res = {};
@@ -23,53 +26,103 @@ function path_parse(path){
     res.order = path.split('\\');
     res.root_index = res.order.findIndex(element => element === ROOT) + 2;
     res.ws_order = res.order.concat().slice(res.root_index, res.order.length - 1);
-    // res.svc_path = res.order.concat();
+
     var tmp_order = res.order.concat();
     tmp_order.splice(res.root_index - 1, 1, svc_name);
     res.svc_path = tmp_order.join('\\');
+
     res.name = res.order[res.order.length -1];
     res.base_name = res.name.replace('.'+option.ext, '');
     res.root = res.order[0];
     return res;
 }
 
-fl.read([WS], option, function(rs){
-    for(var i=0; i<rs.length; i++){
-        var file = rs[i];
-        // console.log(file);
-        var item = path_parse(file.path);
-        console.log(item);
-
-        var wdir = svc_name;
-        for(var j=0; j<item.ws_order.length; j++){
-            wdir = path.join(wdir, item.ws_order[j]);
+function main_init(){
+    fl.read([WS], option, function(rs){
+        for(var i=0; i<rs.length; i++){
+            var file = rs[i];
+            // console.log(file);
+            var item = path_parse(file.path);
+            console.log(item);
+    
+            var wdir = svc_name;
+            for(var j=0; j<item.ws_order.length; j++){
+                wdir = path.join(wdir, item.ws_order[j]);
+                if (!fs.existsSync(wdir)) {
+                    fs.mkdirSync(wdir);
+                }
+            }
+            wdir = path.join(wdir, item.name);
             if (!fs.existsSync(wdir)) {
                 fs.mkdirSync(wdir);
             }
+            exec('unzip -o ' + item.path + ' -d ' + item.svc_path, (err, stdout, stderr) => {
+                if (err) { throw err }
+                console.log(`stdout: ${stdout}`);
+            });
         }
-        wdir = path.join(wdir, item.name);
-        if (!fs.existsSync(wdir)) {
-            fs.mkdirSync(wdir);
-        }
-        // fs.createReadStream(item.path).pipe( unzip.Extract( { path: wdir } ) );
-        // exec('cd ' + wdir);
-        exec('unzip ' + item.path + ' -d ' + item.svc_path, (err, stdout, stderr) => {
-            if (err) { throw err }
-            console.log(`stdout: ${stdout}`);
-        });
-        // exec('pwd', (err, stdout, stderr) => {
-        //     if (err) { throw err }
-        //     console.log(`stdout: ${stdout}`);
-        // });
+    });
+}
 
+function main_decompose(){
+    console.log('comming soon. decompose!!');
+}
 
-        // exec('cd ' + wdir, (error, stdout, stderr) => {
-        //     if (err) { throw err }
-        // });
-        // exec('pwd', (error, stdout, stderr) => {
-        //     if (err) { throw err }
-        //     console.log(`stdout: ${stdout}`);
-        //     // console.error(`stderr: ${stderr}`);
-        // });
-    }
+function main_commit(){
+    console.log('comming soon. commit!!');
+}
+
+function main_deploy(){
+    console.log('comming soon. deploy!!');
+}
+
+program
+  .name('tableau src vertion control')
+  .version('0.0.1', '-v, --version, version')
+  .option("-h, --help, help", "Show help.")
+
+program.command('init')
+  .description('git repo init.')
+  .action((str, options)=> {
+    main_init();
 });
+
+program.command('decompose')
+  .description('ws prep files to svc file decompose.')
+  .alias('d')
+  .action((str, options)=> {
+    main_decompose();
+});
+
+program.command('commit')
+  .description('git commit.')
+  .alias('c')
+  .action((str, options)=> {
+    main_commit();
+});
+
+program.command('deploy')
+  .description('samthing enviroment prep deployment. at deploy.conf')
+  .argument('[env]', 'deploy enviroment.')
+  .alias('d')
+  .action((str, options)=> {
+    main_deploy();
+});
+
+program.command('dev')
+  .description('show dev parameters')
+  .action((str, options)=> {
+    what_params();
+});
+
+program.parse();
+
+//   .command("init", "git repo init.")
+//   .command("decompose", "ws prep files to svc file decompose.")
+//   .command("commit", "git commit.")
+//   .command("deploy [env]", "samthing enviroment prep deployment. at deploy.conf", (value) => { return (value || []).split(","); }, [])
+
+
+if (process.argv.length < 3) {
+  program.help();
+}
